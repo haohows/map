4d55acb3cc841c9d568459981ca22205522be2f6
Hao <faraway520@gmail.com>
2020-08-13 09:27:38 +0800

    更改打包位置

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './map'),
+    path: path.resolve(__dirname, './dist'),
     publicPath: '/map/',
     filename: 'build.js'
   },
